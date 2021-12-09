20f33518d1d517ea067fa4ce1d315068c4f5d65c
Rupesh Kumar <rkumar@palo-it.com>
2021-12-09 18:07:34 +0800

    service bus added

---
+++ demo-servicebus/demo-servicebus/src/main/java/org/palo/FileDetails.java
@@ -0,0 +1,15 @@
+package org.palo;
+
+public class FileDetails {
+
+    private String fileContent;
+
+    public String getFileContent() {
+        return fileContent;
+    }
+
+    public void setFileContent(String fileContent) {
+        this.fileContent = fileContent;
+    }
+
+}
