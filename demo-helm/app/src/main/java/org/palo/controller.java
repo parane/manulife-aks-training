b8e8ae82b84ddc2bee6e0a8223ae1f436f7de363
Sheila Graza <sgraza@palo-it.com>
2021-12-20 08:38:07 +0800

    update sourcecode demo-basic-aks

--- demo-helm/app/src/main/java/org/palo/controller.java
+++ demo-helm/app/src/main/java/org/palo/controller.java
@@ -7,12 +7,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class controller {
 
-	@Value("${spring.application.name}")
+	@Value("${SPRING_APPLICATION_NAME}")
 	String appName;
+
+	@Value("${SPRING_APPLICATION_ENV}")
+	String appEnv;
+
 
 	@GetMapping("/appname")
 	public String appName() {
 		return appName;
+	}
+
+	@GetMapping("/appenv")
+	public String appEnv() {
+		return appEnv;
 	}
 
 	@GetMapping("/")
